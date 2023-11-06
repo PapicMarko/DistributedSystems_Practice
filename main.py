e31d712501ddeb4f84f5c58c1ae7edd86c1ad976
Marko Papic <32815437+PapicMarko@users.noreply.github.com>
2023-11-06 12:15:11 +0100

    Updejtana verzija main-a

--- main.py
+++ main.py
@@ -1,35 +1,63 @@
 import fastapi
 import requests
 import time
+import asyncio
 import httpx
 
-from multiprocessing import ThreadPool
+from multiprocessing.pool import ThreadPool
 
 app = fastapi.FastAPI()
 
+
 def pozovi_fib(x):
     t1 = time.time()
-    response = requests.get(f"http://localhost:8001/fib/{x}")
-    rezultat = response.json()["result"]
+    response = requests.get(f"http://localhost:8000/fib/{x}")
+    rezultat = response.json()
+    # rezultat = {
+    #   "input": 10,
+    #   "result": 55
+    # }
+    print("JSON", rezultat)
+
+    pravi_rezulatat = rezultat["result"]
     t2 = time.time()
+
     trajanje = (t2 - t1) * 1000.0
+    print(f"Pozivanje fib({x}) je trajalo {trajanje:.2f}ms")
 
-    print(f"Pozivanje fib({x}) je trajalo {trajanje:.2f} ms")
+    return pravi_rezulatat
 
-    return rezultat
+
+async def get_fib(x):
+    async with httpx.AsyncClient() as client:
+        url = f"http://127.0.0.1:8000/fib/{x}"
+        response = await client.get(url)
+        rezultat = response.json()["result"]
+        print(f"Rezultat za {x} je {rezultat}")
+        return rezultat
+
 
 @app.get("/zbroj_fib/{n}")
 async def zbroj_fib(n: int):
-    rezultat = pozovi_fib(n)
-    return {"input": n, "result": rezultat}
+    # pozivati drugi servis da bi izračunala sumu prvih
+    # N fib brojeva
 
-    with ThreadPool(processes = 100) as pool:
-        rezultat = pool.map(pozovi_fib, [0, 1, 2, 3])
+    # npr. primi 3 kao argument
+    # mora vratiti fib(1) + fib(2) + fib(3)
+    # primi 10 kao argument
+    # mora vratiti fib(1) + fib(2) + ... + fib(10)
+    # prevedeno        1  +     2  + ... +     55
 
-        print(rezultat)
+    parametri = [get_fib(x) for x in range(n + 1)]
 
-    rezultat = 0
-    for x in range(n):
-        rezultat += pozovi_fib(x)
+    #    * - spreading operator
+    rezultat = await asyncio.gather(*parametri)
 
-    return {"input": n, "result": rezultat}
+    # rezultat = 0
+    # for x in range(n + 1):
+    #     rezultat += await get_fib(x)
+
+    return {"input": n, "result": sum(rezultat)}
+
+
+# Pitanje: koliko traje izvođenje za N=100?
